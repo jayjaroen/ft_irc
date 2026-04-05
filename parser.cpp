7bfe0ee62542b24d8f4458f6728d2548d0eab05f
Tyto Vulpes <164847370+42psenalia@users.noreply.github.com>
2026-04-05 17:20:44 +0700

    parser before handling parameters
    changes to command identifications to types and codes, for more readability

---
+++ parser.cpp
@@ -0,0 +1,97 @@
+#include "parser.hpp"
+
+Command::Command(){}
+Command::~Command(){}
+
+std::string	commandcheck(std::string input)
+{
+	size_t	len = 0;
+	while (!isspace(input[len]))
+		len++;
+	std::string	word = input.substr(0, len);
+	std::string commlist[] = {"CAP", "PASS", "NICK", "USER", "QUIT", "PING", "JOIN",
+		"PART", "CONNECT", "PRIVMSG", "MODE", "KICK"};
+	for (size_t i = 0; i < 12; i++)
+	{
+		if (word == commlist[i])
+			return word;
+	}
+	return "";
+}
+
+size_t	Command::idcomms(const std::string word)
+{
+	std::string	connectmess[] = {"CAP", "AUTHENTICATE", "PASS", "NICK", "USER", "PING",
+		"PONG", "OPER", "QUIT", "ERROR"};
+	std::string	channelmess[] = {"JOIN", "PART", "TOPIC", "NAME", "LIST", "INVITE", "KICK"};
+	std::string	servmess[] = {"MOTD", "VERSION", "ADMIN", "CONNECT", "LUSERS", "TIME",
+		"STATS", "HELP", "INFO", "MODE"};
+	std::string	usermess[] = {"PRIVMSG", "NOTICE", "WHO", "WHOIS", "WHOWAS"};
+	std::string	opermess[] = {"KILL", "REHASH", "RESTART", "SQUIT"};
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		if (word == connectmess[i])
+		{
+			this->type = 1;
+			this->code = i;
+			return (word.length());
+		}
+	}
+	for (size_t i = 0; i < 7; i++)
+	{
+		if (word == channelmess[i])
+		{
+			this->type = 2;
+			this->code = i;
+			return (word.length());
+		}
+	}
+	for (size_t i = 0; i < 10; i++)
+	{
+		if (word == servmess[i])
+		{
+			this->type = 3;
+			this->code = i;
+			return (word.length());
+		}
+	}
+	for (size_t i = 0; i < 5; i++)
+	{
+		if (word == usermess[i])
+		{
+			this->type = 4;
+			this->code = i;
+			return (word.length());
+		}
+	}
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (word == opermess[i])
+		{
+			this->type = 5;
+			this->code = i;
+			return (word.length());
+		}
+	}
+}
+
+void	Command::msgparser(const std::string input)
+{
+	// ConnectMess	type1;
+	// ChannelMess	type2;
+	// ServMess	type3;
+	// UserMess	type4;
+	// OperMess	type5;
+	std::string	icomm = commandcheck(input);
+	if (!icomm.empty())
+		this->params[0][0] = icomm;
+	else
+	{
+		// size_t	len = this->idcomms(icomm);
+		// for (size_t i = 0; input[len + i]; i++)
+		// {
+		// 	if (input[len + i] == ",")
+		// }
+	}
+}
---
+++ parser.hpp
@@ -0,0 +1,117 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <functional>
+
+struct IRCMessage{
+	std::string prefix;
+	std::string command;
+	std::vector<std::string> params;
+	std::string trailing;
+};
+
+class Command
+{
+	private:
+		// std::string	command;
+		size_t		type;
+		size_t		code;
+		// typedef void (Command::*Handler)(const IRCMessage&, Client&);
+		// std::map<std::string, Handler> _hanlers;
+		std::string	**params; //2 pointer for [array set #no][string #no]
+	public:
+		Command();
+		~Command();
+		// void intitHandlers();
+		// void execute_comand(const IRCMessage & msg);
+		void	msgparser(const std::string input);
+		size_t	idcomms(const std::string word);
+		// void handleChannelOp(ChannelMess cmd, const IRCMessage& msg);
+		// void handleUserComm(UserMess cmd, const IRCMessage& msg);
+		// void handleConnection(ConnectMess cmd, const IRCMessage& msg);
+		// void sendResponse(int fd, const std::string& response);
+};
+
+// class EnviVar
+// {
+// 	private:
+// 		char	**envmod;
+// 		EnviVar();
+// 	public:
+// 		~EnviVar();
+// 		char	**envparser(const char **env);
+// };
+
+enum ConnectMess
+{
+	CAP,
+	AUTHENTICATE,
+	PASS,
+	NICK,
+	USER,
+	PING,
+	PONG,
+	OPER,
+	QUIT,
+	ERROR
+};
+
+enum ChannelMess
+{
+	JOIN,
+	PART,
+	TOPIC,
+	NAMES,
+	LIST,
+	INVITE,
+	KICK
+};
+
+enum ServMess
+{
+	MOTD,
+	VERSION,
+	ADMIN,
+	CONNECT,
+	LUSERS,
+	TIME,
+	STATS,
+	HELP,
+	INFO,
+	MODE
+};
+
+enum UserMess
+{
+	PRIVMSG,
+	NOTICE,
+	WHO,
+	WHOIS,
+	WHOWAS
+};
+
+enum OperMess
+{
+	KILL,
+	REHASH,
+	RESTART,
+	SQUIT
+};
+
+// enum NumRpl
+// {
+// 	UNKNOWN_CMD,
+// 	RPL_WELCOME,
+// 	.
+// 	.
+// 	.
+// };
+
+
+// IRCMessage translateFromParser(char ***params);
+
+#endif
