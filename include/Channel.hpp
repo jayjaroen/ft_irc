2cb6d61e4615a7cb621f0f48d971935dd63d4a44
jayjaroen <jay.ji.jaroen@gmail.com>
2026-05-01 17:50:23 +0700

    add channel

---
+++ include/Channel.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 12:23:29 by jjaroens          #+#    #+#             */
+/*   Updated: 2026/04/25 13:53:26 by jjaroens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+# include "Client.hpp"
+
+class Client;
+
+class Channel
+{
+    private:
+		std::string				_name;
+		Client*					_admin;
+		std::vector<Client *>	_clients;
+
+		/* Channel modes*/
+		std::string				_k; //channel key
+		size_t					_l; //limit number of channel members
+		bool					_n; // yes/no to external msgs
+
+		Channel();
+		Channel(const Channel &other);
+	
+	public:
+		Channel(const std::string &name, const std::string &key, Client *admin);
+		~Channel();
+		
+		std::string				get_name() const;
+		Client*					get_admin() const;
+		std::string				get_key() const;
+		size_t					get_limit();
+		bool					get_ext_msg();
+		size_t					get_channel_size();
+
+		void					set_key(std::string key);
+		void					set_limit(size_t limit);
+		void					set_ext_msg(bool flag);
+		
+		/*Channel actions*/
+		void					broadcast(const std::string &message);
+		// void					broadcast(const std::string &message, Client* exclude);
+		void					add_client(Client* client);
+		void					remove_client(Client* client);
+		//kick?
+		
+		
+		
+};
+
+#endif
---
+++ srcs/Channel.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 12:23:39 by jjaroens          #+#    #+#             */
+/*   Updated: 2026/04/25 13:59:39 by jjaroens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/Channel.hpp"
+
+Channel::Channel(){}
+Channel::~Channel(){}
+
+Channel::Channel(const std::string &name, const std::string &key, Client* admin)
+    :_name(name), _admin(admin), _k(key), _l(0), _n(false)
+{
+    
+}
+
+std::string     Channel::get_name() const
+{
+    return _name;
+}
+
+Client*         Channel::get_admin() const
+{
+    return _admin;
+}
+
+std::string     Channel::get_key() const
+{
+    return _k;
+}
+
+size_t          Channel::get_limit()
+{
+    return _l;
+}
+
+bool            Channel::get_ext_msg()
+{
+    return _n;
+}
+
+size_t          Channel::get_channel_size()
+{
+    return _clients.size();
+}
+
+void            Channel::set_key(std::string key)
+{
+    _k = key;
+}
+
+void            Channel::set_limit(size_t limit)
+{
+    _l = limit;
+}
+
+void            Channel::set_ext_msg(bool flag)
+{
+    _n = flag;
+}
+
+void            Channel::broadcast(const std::string &message)
+{
+    std::vector<Client*>::iterator it_start = _clients.begin();
+    std::vector<Client*>::iterator it_end = _clients.end();
+    
+    while (it_start != it_end)
+    {
+        (*it_start)->write(message); //client write message
+        it_start++;
+    }
+}
+
+//write exclude
